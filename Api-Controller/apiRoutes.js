471deccf04861eea12cf7e3bbe5ba5b24ab77327
Khizar khan <Engrkhizarkhan1@gmail.com>
2025-02-01 02:59:45 +0500

    unique removed

--- Api-Controller/apiRoutes.js
+++ Api-Controller/apiRoutes.js
@@ -20,7 +20,7 @@ connectDB();
 // Schema
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true, minlength: 3 , maxlength: 50 },
-    email: { type: String, required: true, unique: true, trim: true, minlength: 5, maxlength: 50 },
+    email: { type: String, required: true, trim: true, minlength: 5, maxlength: 50 },
     subject: { type: String, required: true, trim: true, minlength: 5, maxlength: 50 },
     message: { type: String, required: true, trim: true, minlength: 10},
     Timestamp: { type: Date, default: Date.now }
